4219daa2b43be3763c5b2f6a3fa517b04babce07
nethen <72537484+nethen@users.noreply.github.com>
2025-04-01 14:25:50 -0700

    feat: added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,57 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const rightGrotesk = localFont({
+  src: [
+    {
+      path: "./fonts/PPRightGroteskText-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPRightGroteskText-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPRightGrotesk-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPRightGrotesk-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPRightGrotesk-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/PPRightGrotesk-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-right-grotesk",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const rightGothic = localFont({
+  src: [
+    {
+      path: "./fonts/PPRightGothic-SpatialBlackItalic.woff",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "./fonts/PPRightGothic-SpatialBlackItalic.woff2",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-right-gothic",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +67,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${rightGothic.variable} ${rightGrotesk.variable} antialiased`}
       >
         {children}
       </body>
