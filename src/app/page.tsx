af756043f4d4225fde552922bf364f2c410980f5
nethen <72537484+nethen@users.noreply.github.com>
2025-04-01 16:47:07 -0700

    feat: theme variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
             Strikeout
           </h1>
           <h2 className="font-bold text-lg">Sending startups to stardom</h2>
-          <button className="p-4">Vote now</button>
         </div>
       </header>
       <main className="flex flex-col text-xl px-6 py-6">
@@ -47,8 +46,175 @@ export default function Home() {
           <p className="font-bold">
             Strikeout something something solvees this idk.
           </p>
+          <button className="flex items-stretch w-full">
+            <svg
+              width="28"
+              height="52"
+              viewBox="0 0 28 52"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-full"
+            >
+              <rect
+                width="14"
+                height="52"
+                transform="matrix(-1 0 0 1 28 0)"
+                fill="url(#paint0_linear_1391_3579)"
+                // style=""
+              />
+              <path
+                d="M28 -1.39246e-06L14.0057 26H-4.76827e-07L14.0057 -1.39246e-06H28Z"
+                fill="url(#paint1_linear_1391_3579)"
+                // style=""
+              />
+              <path
+                d="M0 26L13.9943 52H28L13.9943 26H0Z"
+                fill="url(#paint2_linear_1391_3579)"
+                // style=""
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_1391_3579"
+                  x1="14"
+                  y1="26"
+                  x2="0"
+                  y2="26"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop
+                    stopColor="#F47095"
+                    // style="stopColor:#F47095;stopColor:color(display-p3 0.9569 0.4392 0.5843);stop-opacity:1;"
+                  />
+                  <stop
+                    offset="1"
+                    stopColor="#FE9CB7"
+                    // style="stopColor:#FE9CB7;stopColor:color(display-p3 0.9961 0.6118 0.7176);stop-opacity:1;"
+                  />
+                </linearGradient>
+                <linearGradient
+                  id="paint1_linear_1391_3579"
+                  x1="19.7016"
+                  y1="26.01"
+                  x2="19.7016"
+                  y2="1.12035e-07"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop
+                    stopColor="#FF9CB8"
+                    // style="stopColor:#FF9CB8;stopColor:color(display-p3 1.0000 0.6118 0.7216);stop-opacity:1;"
+                  />
+                  <stop
+                    offset="1"
+                    stopColor="#C31344"
+                    // style="stopColor:#C31344;stopColor:color(display-p3 0.7647 0.0745 0.2667);stop-opacity:1;"
+                  />
+                </linearGradient>
+                <linearGradient
+                  id="paint2_linear_1391_3579"
+                  x1="8.29837"
+                  y1="52.01"
+                  x2="8.29837"
+                  y2="26"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop
+                    stopColor="#FF9CB8"
+                    // style="stopColor:#FF9CB8;stopColor:color(display-p3 1.0000 0.6118 0.7216);stop-opacity:1;"
+                  />
+                  <stop
+                    offset="1"
+                    stopColor="#C31344"
+                    // style="stopColor:#C31344;stopColor:color(display-p3 0.7647 0.0745 0.2667);stop-opacity:1;"
+                  />
+                </linearGradient>
+              </defs>
+            </svg>
+            <div className="flex justify-center items-center bg-gradient-to-r flex-auto from-theme-light via-[#FFD0DD] text-theme-dark font-medium to-theme-light">
+              Vote now
+            </div>
+            <svg
+              width="28"
+              height="52"
+              viewBox="0 0 28 52"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                width="14"
+                height="52"
+                fill="url(#paint0_linear_1391_3579)"
+                // style=""
+              />
+              <path
+                d="M0 -1.39246e-06L13.9943 26H28L13.9943 -1.39246e-06H0Z"
+                fill="url(#paint1_linear_1391_3579)"
+                // style=""
+              />
+              <path
+                d="M28 26L14.0057 52H-4.76827e-07L14.0057 26H28Z"
+                fill="url(#paint2_linear_1391_3579)"
+                // style=""
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_1391_3579"
+                  x1="14"
+                  y1="26"
+                  x2="0"
+                  y2="26"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop
+                    stopColor="#F47095"
+                    // style="stopColor:#F47095;stopColor:color(display-p3 0.9569 0.4392 0.5843);stop-opacity:1;"
+                  />
+                  <stop
+                    offset="1"
+                    stopColor="#FE9CB7"
+                    // style="stopColor:#FE9CB7;stopColor:color(display-p3 0.9961 0.6118 0.7176);stop-opacity:1;"
+                  />
+                </linearGradient>
+                <linearGradient
+                  id="paint1_linear_1391_3579"
+                  x1="8.29837"
+                  y1="26.01"
+                  x2="8.29837"
+                  y2="1.12035e-07"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop
+                    stopColor="#FF9CB8"
+                    // style="stopColor:#FF9CB8;stopColor:color(display-p3 1.0000 0.6118 0.7216);stop-opacity:1;"
+                  />
+                  <stop
+                    offset="1"
+                    stopColor="#C31344"
+                    // style="stopColor:#C31344;stopColor:color(display-p3 0.7647 0.0745 0.2667);stop-opacity:1;"
+                  />
+                </linearGradient>
+                <linearGradient
+                  id="paint2_linear_1391_3579"
+                  x1="19.7016"
+                  y1="52.01"
+                  x2="19.7016"
+                  y2="26"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop
+                    stopColor="#FF9CB8"
+                    // style="stopColor:#FF9CB8;stopColor:color(display-p3 1.0000 0.6118 0.7216);stop-opacity:1;"
+                  />
+                  <stop
+                    offset="1"
+                    stopColor="#C31344"
+                    // style="stopColor:#C31344;stopColor:color(display-p3 0.7647 0.0745 0.2667);stop-opacity:1;"
+                  />
+                </linearGradient>
+              </defs>
+            </svg>
+          </button>
         </div>
-        <article>
+        {/* <article>
           <h2 className="font-bold uppercase text-sm mb-4">Past contestants</h2>
           <ul>
             <li>
@@ -57,9 +223,9 @@ export default function Home() {
               </div>
             </li>
           </ul>
-        </article>
+        </article> */}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -105,7 +271,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
