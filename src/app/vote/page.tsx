f146ec7d8d58641682966c63b7b4df319bbcfcc5
nethen <72537484+nethen@users.noreply.github.com>
2025-04-01 17:26:22 -0700

    feat: voting layout/page

---
+++ src/app/vote/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col text-center gap-4 mb-12">
+        <h2>Select your favorite startup</h2>
+        <section>
+          <div>
+            <div className="w-full h-48 bg-slate-500 text-white p-4">
+              <h3>Hangover</h3>
+            </div>
+          </div>
+          <span>VS.</span>
+          <div>
+            <div className="w-full h-48 bg-slate-500 text-white p-4">
+              <h3>Hangover</h3>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
