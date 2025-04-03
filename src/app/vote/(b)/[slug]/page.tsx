4dbf456384fd0608e8ac590d5c93de2136748cd1
nethen <72537484+nethen@users.noreply.github.com>
2025-04-03 03:07:16 -0700

    fix: back button

--- src/app/vote/(b)/[slug]/page.tsx
+++ src/app/vote/(b)/[slug]/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { VoteModal } from "@/app/components/Invest/Modal/VoteModal";
 import { useState } from "react";
 import { AnimatePresence } from "motion/react";
+import { useRouter } from "next/navigation";
 // import { useState } from "react";
 
 // type PageProps = {
@@ -17,6 +18,7 @@ import { AnimatePresence } from "motion/react";
 export default function Page() {
   const params = useParams();
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
   const startup = Object.values(startupLocalData).find(
     (s) => s.url === params.slug
   );
@@ -42,6 +44,24 @@ export default function Page() {
           )}
         </AnimatePresence>
         <section className="flex flex-col gap-6">
+          <button
+            type="button"
+            onClick={() => router.back()}
+            className="cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="size-10"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
           <div className="aspect-square w-full overflow-hidden rounded-2xl">
             <Image
               src={startup.image}
