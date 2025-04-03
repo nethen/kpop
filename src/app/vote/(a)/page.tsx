5cdbcc89675ac21b8c8d7fbee736ca5918ba13b2
nethen <72537484+nethen@users.noreply.github.com>
2025-04-03 02:31:20 -0700

    style: added image previews for startups

--- src/app/vote/(a)/page.tsx
+++ src/app/vote/(a)/page.tsx
@@ -1,17 +1,15 @@
 import Link from "next/link";
-import { getStartups } from "@/app/components/Neon/actions";
-import {startupLocalData} from "@/app/vote/(b)/[slug]/data"
-import { start } from "repl";
+import { startupLocalData } from "@/app/vote/(b)/[slug]/data";
+import Image from "next/image";
 export default async function Page() {
   // const router = useRouter();
 
-  // const startupLocalData = 
+  // const startupLocalData =
 
   // const startups = await getStartups().then((response) => {
   //   return response?.sort((a, b) => a.id - b.id);
   // });
   // console.log(startups);
-  const startups = true
 
   return (
     <>
@@ -19,10 +17,20 @@ export default async function Page() {
         <h2 className="font-bold text-2xl">Select your favorite startup</h2>
         <section className="flex flex-col gap-4">
           <div>
-            {startups && (
+            {startupLocalData && (
               <div className="w-full bg-zinc-900 border border-zinc-800 rounded-xl text-white p-4 flex flex-col text-left">
-                <div className="rounded-md bg-pink-300 w-full aspect-video mb-4" />
-                <h3 className="font-bold text-2xl">{startupLocalData.AiDol.name}</h3>
+                <div className="rounded-md w-full aspect-video mb-4 overflow-hidden">
+                  <Image
+                    src={startupLocalData.AiDol.image}
+                    alt="cover photo"
+                    width={500}
+                    height={500}
+                    className="size-full object-cover object-top"
+                  />
+                </div>
+                <h3 className="font-bold text-2xl">
+                  {startupLocalData.AiDol.name}
+                </h3>
                 <p>{startupLocalData.AiDol.description}</p>
                 <Link
                   className="px-4 py-2 text-lg bg-theme-light text-theme-dark rounded-md font-bold mt-4 cursor-pointer w-fit"
@@ -39,10 +47,20 @@ export default async function Page() {
             <div className="border-transparent border-l-[clamp(3rem,25vw,12rem)] border-y-2 border-l-white h-0 w-0" />
           </div>
           <div>
-            {startups && (
+            {startupLocalData && (
               <div className="w-full bg-zinc-900 border border-zinc-800 rounded-xl text-white p-4 flex flex-col text-left">
-                <div className="rounded-md bg-pink-300 w-full aspect-video mb-4" />
-                <h3 className="font-bold text-2xl">{startupLocalData.Uptick.name}</h3>
+                <div className="rounded-md w-full aspect-video mb-4 overflow-hidden">
+                  <Image
+                    src={startupLocalData.Uptick.image}
+                    alt="cover photo"
+                    width={500}
+                    height={500}
+                    className="size-full object-cover object-top"
+                  />
+                </div>
+                <h3 className="font-bold text-2xl">
+                  {startupLocalData.Uptick.name}
+                </h3>
                 <p>{startupLocalData.Uptick.description}</p>
                 <Link
                   className="px-4 py-2 text-lg bg-theme-light text-theme-dark rounded-md font-bold mt-4 cursor-pointer w-fit"
